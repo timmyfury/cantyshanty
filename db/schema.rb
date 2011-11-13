27b5abb80e7cef8439c2299d4cfd8d52133ed41e
Tim Crowe <tim.crowe@timmyfury.com>
2011-11-13 14:15:20 -0500

    enhanced admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111113032218) do
+ActiveRecord::Schema.define(:version => 20111113180352) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(:version => 20111113032218) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "legacy_slug"
+    t.boolean  "publishable",        :default => false
   end
 
   create_table "taggings", :force => true do |t|
