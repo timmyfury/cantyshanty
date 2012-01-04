0ae6f5b60325bb48ad1acb7646f081919734dea8
Tim Crowe <tim.crowe@timmyfury.com>
2012-01-04 17:37:27 -0500

    adding auto tweet to publish

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key = 'fYjCxNhPjBFh531COOqt7Q'
+  config.consumer_secret = 'yaTU7EML591NSf6jTfdKcSmo2HzMx5e0Jtn2TrvVBfE'
+  config.oauth_token = '90432973-MSzGQJH891G1RBrcyEDybj8TwB7GHEQil1moeM3Eb'
+  config.oauth_token_secret = 'pqX9xopSyjsS7uKV8IB3LdS3z144vkgAXEVnuPaQ'
+end
