4ad6ad4b3d658ad8031cfdef9c4a81d0a0a67824
Tim Crowe <tim.crowe@timmyfury.com>
2012-01-05 14:02:13 -0500

    catching routing errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Cantyshanty::Application.routes.draw do
-  root :to => "home#index"
+
   match 'tag/:slug' => 'home#tag', :as => :tag
   match 'tag' => 'home#tag_cloud', :as => :tag_cloud
   match '_:slug' => 'home#image', :as => :short
@@ -12,60 +12,10 @@ Cantyshanty::Application.routes.draw do
     put 'publish', :on => :member
   end
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
+  root :to => "home#index"
+  
+  unless Cantyshanty::Application.config.consider_all_requests_local
+    match '*a', :to => 'application#render_not_found'
+  end
 
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
 end
