5f1edc83f835fd8bb9ae3d888c9eef7daf3ed35b
Tim Crowe <tim.crowe@timmyfury.com>
2012-01-05 14:27:37 -0500

    adding source fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111113180352) do
+ActiveRecord::Schema.define(:version => 20120105150554) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(:version => 20111113180352) do
     t.datetime "updated_at"
     t.string   "legacy_slug"
     t.boolean  "publishable",        :default => false
+    t.string   "source_title"
+    t.string   "source_url"
   end
 
   create_table "taggings", :force => true do |t|
